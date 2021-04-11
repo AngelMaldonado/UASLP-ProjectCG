b295e0b6ca62a0348c5f5f130a0bae7c93ceb389
AngelMaldonado <amaldonadojuarez@gmail.com>
2021-04-11 17:59:00 -0500

    Advance on makefile

--- test/trajectories/TrajectoriesTest.cpp
+++ test/trajectories/TrajectoriesTest.cpp
@@ -1,24 +1,29 @@
 #include "../../include/trajectories.h"
 #include "../../include/glinterface.h"
 
-int main(int argc, char* argv[]) {
 
-    return 0;
+TrajectoryHandler trajectoryHandler;
+float x1, y_1, x2, y2;
+void display() {
+    trajectoryHandler.geoDrawLine(x1, y_1, x2, y2, 1, 1, 1);
 }
 
 void givenX1Y1X2Y2_whenMIsEqualUnder1_thenLineDraws(int argc, char* argv[]) {
     // Given
     GLInterface glInterface = GLInterface();
-    TrajectoryHandler trajectoryHandler;
-    float x1, y1, x2, y2;
 
     // When
     glInterface.initGLWindow(argc, argv, 300, 300, 100, 100);
     glInterface.initView();
-    x1 = 1; y1 = 2;
+    x1 = 1; y_1 = 2;
     x2 = 9; y2 = 4;
 
     // Then
-    trajectoryHandler.geoDrawLine(x1, y1, x2, y2, 1, 1, 1);
+    glInterface.setDisplayFunction(display);
     glInterface.startGLloop();
+}
+
+int main(int argc, char* argv[]) {
+    givenX1Y1X2Y2_whenMIsEqualUnder1_thenLineDraws(argc, argv);
+    return 0;
 }
