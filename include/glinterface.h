114bd2f8bbf2848b347bc7fe4e07b9e5cfab3cb9
angelmaldonado <amaldonadojuarez@gmail.com>
2021-04-13 14:58:50 -0500

    #2 - Makefile complete - Need to implement 2D and 3D views in GLInterface

--- include/glinterface.h
+++ include/glinterface.h
@@ -4,17 +4,30 @@
 /**
  * To use this interface subclasses are necessary to use the callbacks
  */
+enum View {
+    VIEW_3D,
+    VIEW_2D
+};
+
 class GLInterface {
     protected:
         /*
-         * This is an instance pointer of GLInterface class to be able to to instantiance the
+         * This is an instance pointer of GLInterface class to be able to instantiante the
          * function calls.
          */
         static GLInterface *instance;
     private:
+        // Window values
         int wndWith, wndHeight;
         int wndPosX, wndPosY;
         char* wndName;
+        GLenum matrixMode;
+        float orthoVleft, orthoVright, orthoVbottom, orthoVtop;
+        float nearVal, farVal;
+        float rotAngle, rotX, rotY, rotZ;
+
+        // This variable sets the 3D or 2D view of the framework
+        View view;
     public:
         // Empty GLInterface constructor
         GLInterface();
