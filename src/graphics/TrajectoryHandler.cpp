16ca9138bb4dac5767e65c11fb7e9b7e32c14334
angelmaldonado <amaldonadojuarez@gmail.com>
2021-04-14 16:14:42 -0500

    #2 - Linear drawings ready

--- src/graphics/TrajectoryHandler.cpp
+++ src/graphics/TrajectoryHandler.cpp
@@ -1,11 +1,23 @@
 #include "../../include/trajectories.h"
-#include <iostream>
+
+void TrajectoryHandler::initDrawingTrajectory() {
+    // Set the line color and point size
+    glColor3f(red, green, blue);
+    glPointSize(pointSize);
+}
+
+float TrajectoryHandler::slope(float x1, float y1, float x2, float y2) {
+    float m = (y2 - y1) / (x2 - x1);
+    return m;
+}
 
 TrajectoryHandler::TrajectoryHandler() {
     fx1 = fy1 = fx2 = fy2 = -1;
     fincrement = 0.05;
     ix1 = iy1 = ix2 = iy2 = -1;
     red = green = blue = 1;
+    pointSize = 1;
+    lineStyle = GL_POINTS;
     algorithm = ALG_GEO_DRAW_LINE;
 }
 
@@ -18,8 +30,19 @@ void TrajectoryHandler::setfCoordinates(float fx1, float fy1, float fx2, float f
     this->fx2 = fx2; this->fy2 = fy2;
 }
 
+void TrajectoryHandler::setiCoordinates(int ix1, int iy1, int ix2, int iy2) {
+    this->ix1 = ix1; this->iy1 = iy1;
+    this->ix2 = ix2; this->iy2 = iy2;
+}
+
 void TrajectoryHandler::setfIncrement(float fincrement) {
     this->fincrement = fincrement;
+}
+
+void TrajectoryHandler::setDrawingValues(float pointSize, GLenum lineStyle, int red, int green, int blue) {
+    this->pointSize = pointSize;
+    this->lineStyle = lineStyle;
+    this->red = red; this->green = green; this->blue = blue;
 }
 
 void TrajectoryHandler::display() {
@@ -29,8 +52,10 @@ void TrajectoryHandler::display() {
             geoDrawLine();
         break;
         case ALG_DDA_DRAW_LINE:
+            ddaDrawLine();
         break;
         case ALG_BRSNHM_DRAW_LINE:
+            bresenhamDrawLine();
         break;
     }
 }
@@ -39,20 +64,89 @@ void TrajectoryHandler::geoDrawLine() {
     float m, b, x, y;
 
     // Compute the slope (m)
-    m = (fy2 - fy1) / (fx2 - fx1);
+    m = slope(fx1, fy1, fx2, fy2);
     // Compute b
     b = fy1 - m * fx1;
     
-    // Set the line color and point size
-    glColor3f(red, green, blue);
-    glPointSize(3.0);
+    initDrawingTrajectory();
 
-    glBegin(GL_POINTS);
+    glBegin(lineStyle);
         // Travel from x1 to x2 with some increment and start drawing the points
         for(x = fx1; x <= fx2; x+=fincrement)
         {
             y = m * x + b;
-            glVertex2f(round(x), round(y));
+            glVertex2f(x, y);
+        }
+    glEnd();
+}
+
+void TrajectoryHandler::ddaDrawLine() {
+    float x, y;
+    int dx = ix2 - ix1, dy = iy2 - iy1;
+    int n = max(abs(dx), abs(dy));
+    float dt = n, dxdt = dx / dt, dydt = dy / dt;
+
+    x = ix1;
+    y = iy1;
+    initDrawingTrajectory();
+
+    glBegin(lineStyle);
+        while(n--) {
+            glVertex2i((x), (y));
+            x += dxdt;
+            y += dydt;
+        }
+    glEnd();
+}
+
+void TrajectoryHandler::bresenhamDrawLine() {
+    float x, y;
+    int pk;
+    int dx = ix2 - ix1, dy = iy2 - iy1;
+    int _2dy = 2 * dy, _2dx = 2 * dx;
+    int _2dy_Minus_2dx, _2dx_Minus_2dy;
+    int n = max(abs(dx), abs(dy));
+    float m = slope(ix1, iy1, ix2, iy2);
+    
+    x = ix1;
+    y = iy1;
+    initDrawingTrajectory();
+
+    glBegin(lineStyle);
+        if(m >= 0 && m <= 1) {
+            _2dy_Minus_2dx = _2dy - _2dx;
+            pk = _2dy - dx;
+
+            while(n--) {
+                if(pk < 0)
+                {
+                    glVertex2i(++x, y);
+                    pk = pk + _2dy;
+                }
+                else
+                {
+                    glVertex2i(++x, ++y);
+                    pk = pk + _2dy_Minus_2dx;
+                }
+            }
+        }
+        else if(m > 1)
+        {
+            _2dx_Minus_2dy = _2dx - _2dy;
+            pk = _2dx - dy;
+
+            while(n--) {
+                if(pk < 0)
+                {
+                    glVertex2i(x, ++y);
+                    pk = pk + _2dx;
+                }
+                else
+                {
+                    glVertex2i(++x, ++y);
+                    pk = pk + _2dx_Minus_2dy;
+                }
+            }
         }
     glEnd();
 }
--- test/trajectories/TrajectoriesTest.cpp
+++ test/trajectories/TrajectoriesTest.cpp
@@ -1,11 +1,11 @@
 #include "../../include/trajectories.h"
 
-void givenX1Y1X2Y2_whenMIsEqualUnder1_thenLineDraws(int argc, char* argv[]) {
+void givenX1Y1X2Y2_whenGeometricAlgorithmAndSlopeIs0_thenLineDraws(int argc, char* argv[]) {
     // GIVEN
     TrajectoryHandler* trajectoryHandler = new TrajectoryHandler();
     GLInterface* glInterface = new GLInterface();
-    int x1 = 1; int y1 = 2;
-    int x2 = 9; int y2 = 4;
+    float x1 = 1; float y1 = 2;
+    float x2 = 9; float y2 = 4;
     
     // WHEN
     // TrajectoryHanler automatically computes the slope at geoDrawLine()
@@ -13,13 +13,218 @@ void givenX1Y1X2Y2_whenMIsEqualUnder1_thenLineDraws(int argc, char* argv[]) {
     trajectoryHandler->setfCoordinates(x1, y1, x2, y2);
     trajectoryHandler->setfIncrement(0.00001);
     glInterface->setInstance(trajectoryHandler);
-    glInterface->setView2D(GL_PROJECTION, 0, 10, 0, 10);
+    glInterface->setView2D(GL_PROJECTION, 0, DEF_WINDOW_SIZE, 0, DEF_WINDOW_SIZE);
+
+    // THEN
+    glInterface->startFramework(argc, argv);
+}
+
+void givenX1Y1X2Y2_whenDdaAlgorithmAndSlopeIs0_thenLineDraws(int argc, char* argv[]) {
+    // GIVEN
+    TrajectoryHandler* trajectoryHandler = new TrajectoryHandler();
+    GLInterface* glInterface = new GLInterface();
+    int x1 = 100; int y1 = 250;
+    int x2 = 400; int y2 = 250;
+
+    // WHEN
+    // TrajectoryHanler automatically computes the slope at ddaDrawLine()
+    trajectoryHandler->setAlgorithm(ALG_DDA_DRAW_LINE);
+    trajectoryHandler->setiCoordinates(x1, y1, x2, y2);
+    trajectoryHandler->setDrawingValues(1, GL_POINTS, 1, 1, 1);
+    glInterface->setInstance(trajectoryHandler);
+    glInterface->setView2D(GL_PROJECTION, 0, DEF_WINDOW_SIZE, 0, DEF_WINDOW_SIZE);
+
+    // THEN
+    glInterface->startFramework(argc, argv);
+}
+
+void givenX1Y1X2Y2_whenDdaAlgorithmAndSlopeIsBetween0And1_thenLineDraws(int argc, char* argv[]) {
+    // GIVEN
+    TrajectoryHandler* trajectoryHandler = new TrajectoryHandler();
+    GLInterface* glInterface = new GLInterface();
+    int x1 = 100; int y1 = 100;
+    int x2 = 400; int y2 = 250;
+
+    // WHEN
+    // TrajectoryHanler automatically computes the slope at ddaDrawLine()
+    trajectoryHandler->setAlgorithm(ALG_DDA_DRAW_LINE);
+    trajectoryHandler->setiCoordinates(x1, y1, x2, y2);
+    trajectoryHandler->setDrawingValues(1, GL_POINTS, 1, 1, 1);
+    glInterface->setInstance(trajectoryHandler);
+    glInterface->setView2D(GL_PROJECTION, 0, DEF_WINDOW_SIZE, 0, DEF_WINDOW_SIZE);
+
+    // THEN
+    glInterface->startFramework(argc, argv);
+}
+
+void givenX1Y1X2Y2_whenDdaAlgorithmAndSlopeIs1_thenLineDraws(int argc, char* argv[]) {
+    // GIVEN
+    TrajectoryHandler* trajectoryHandler = new TrajectoryHandler();
+    GLInterface* glInterface = new GLInterface();
+    int x1 = 100; int y1 = 100;
+    int x2 = 400; int y2 = 400;
+
+    // WHEN
+    // TrajectoryHanler automatically computes the slope at ddaDrawLine()
+    trajectoryHandler->setAlgorithm(ALG_DDA_DRAW_LINE);
+    trajectoryHandler->setiCoordinates(x1, y1, x2, y2);
+    trajectoryHandler->setDrawingValues(1, GL_POINTS, 1, 1, 1);
+    glInterface->setInstance(trajectoryHandler);
+    glInterface->setView2D(GL_PROJECTION, 0, DEF_WINDOW_SIZE, 0, DEF_WINDOW_SIZE);
+
+    // THEN
+    glInterface->startFramework(argc, argv);
+}
+
+void givenX1Y1X2Y2_whenDdaAlgorithmAndSlopeIsMoreThan1_thenLineDraws(int argc, char* argv[]) {
+    // GIVEN
+    TrajectoryHandler* trajectoryHandler = new TrajectoryHandler();
+    GLInterface* glInterface = new GLInterface();
+    int x1 = 100; int y1 = 100;
+    int x2 = 300; int y2 = 400;
+
+    // WHEN
+    // TrajectoryHanler automatically computes the slope at ddaDrawLine()
+    trajectoryHandler->setAlgorithm(ALG_DDA_DRAW_LINE);
+    trajectoryHandler->setiCoordinates(x1, y1, x2, y2);
+    trajectoryHandler->setDrawingValues(1, GL_POINTS, 1, 1, 1);
+    glInterface->setInstance(trajectoryHandler);
+    glInterface->setView2D(GL_PROJECTION, 0, DEF_WINDOW_SIZE, 0, DEF_WINDOW_SIZE);
+
+    // THEN
+    glInterface->startFramework(argc, argv);
+}
+
+void givenX1Y1X2Y2_whenDdaAlgorithmAndSlopeIsUndefined_thenLineDraws(int argc, char* argv[]) {
+    // GIVEN
+    TrajectoryHandler* trajectoryHandler = new TrajectoryHandler();
+    GLInterface* glInterface = new GLInterface();
+    int x1 = 100; int y1 = 100;
+    int x2 = 100; int y2 = 400;
+
+    // WHEN
+    // TrajectoryHanler automatically computes the slope at ddaDrawLine()
+    trajectoryHandler->setAlgorithm(ALG_DDA_DRAW_LINE);
+    trajectoryHandler->setiCoordinates(x1, y1, x2, y2);
+    trajectoryHandler->setDrawingValues(1, GL_POINTS, 1, 1, 1);
+    glInterface->setInstance(trajectoryHandler);
+    glInterface->setView2D(GL_PROJECTION, 0, DEF_WINDOW_SIZE, 0, DEF_WINDOW_SIZE);
+
+    // THEN
+    glInterface->startFramework(argc, argv);
+}
+
+void givenX1Y1X2Y2_whenbBresenhamAlgorithmAndSlopeIs0_thenLineDraws(int argc, char* argv[]) {
+    // GIVEN
+    TrajectoryHandler* trajectoryHandler = new TrajectoryHandler();
+    GLInterface* glInterface = new GLInterface();
+    int x1 = 100; int y1 = 250;
+    int x2 = 400; int y2 = 250;
+
+    // WHEN
+    // TrajectoryHanler automatically computes the slope at ddaDrawLine()
+    trajectoryHandler->setAlgorithm(ALG_BRSNHM_DRAW_LINE);
+    trajectoryHandler->setiCoordinates(x1, y1, x2, y2);
+    trajectoryHandler->setDrawingValues(1, GL_POINTS, 1, 1, 1);
+    glInterface->setInstance(trajectoryHandler);
+    glInterface->setView2D(GL_PROJECTION, 0, DEF_WINDOW_SIZE, 0, DEF_WINDOW_SIZE);
+
+    // THEN
+    glInterface->startFramework(argc, argv);
+}
+
+void givenX1Y1X2Y2_whenBresenhamAndSlopeIsBetween0And1_thenLineDraws(int argc, char* argv[]) {
+    // GIVEN
+    TrajectoryHandler* trajectoryHandler = new TrajectoryHandler();
+    GLInterface* glInterface = new GLInterface();
+    int x1 = 100; int y1 = 100;
+    int x2 = 400; int y2 = 250;
+
+    // WHEN
+    // TrajectoryHanler automatically computes the slope at ddaDrawLine()
+    trajectoryHandler->setAlgorithm(ALG_BRSNHM_DRAW_LINE);
+    trajectoryHandler->setiCoordinates(x1, y1, x2, y2);
+    trajectoryHandler->setDrawingValues(1, GL_POINTS, 1, 1, 1);
+    glInterface->setInstance(trajectoryHandler);
+    glInterface->setView2D(GL_PROJECTION, 0, DEF_WINDOW_SIZE, 0, DEF_WINDOW_SIZE);
+
+    // THEN
+    glInterface->startFramework(argc, argv);
+}
+
+void givenX1Y1X2Y2_whenBresenhamAlgorithmAndSlopeIs1_thenLineDraws(int argc, char* argv[]) {
+    // GIVEN
+    TrajectoryHandler* trajectoryHandler = new TrajectoryHandler();
+    GLInterface* glInterface = new GLInterface();
+    int x1 = 100; int y1 = 100;
+    int x2 = 400; int y2 = 400;
+
+    // WHEN
+    // TrajectoryHanler automatically computes the slope at ddaDrawLine()
+    trajectoryHandler->setAlgorithm(ALG_BRSNHM_DRAW_LINE);
+    trajectoryHandler->setiCoordinates(x1, y1, x2, y2);
+    trajectoryHandler->setDrawingValues(1, GL_POINTS, 1, 1, 1);
+    glInterface->setInstance(trajectoryHandler);
+    glInterface->setView2D(GL_PROJECTION, 0, DEF_WINDOW_SIZE, 0, DEF_WINDOW_SIZE);
+
+    // THEN
+    glInterface->startFramework(argc, argv);
+}
+
+void givenX1Y1X2Y2_whenBresenhamAlgorithmAndSlopeIsMoreThan1_thenLineDraws(int argc, char* argv[]) {
+    // GIVEN
+    TrajectoryHandler* trajectoryHandler = new TrajectoryHandler();
+    GLInterface* glInterface = new GLInterface();
+    int x1 = 100; int y1 = 100;
+    int x2 = 300; int y2 = 400;
+
+    // WHEN
+    // TrajectoryHanler automatically computes the slope at ddaDrawLine()
+    trajectoryHandler->setAlgorithm(ALG_BRSNHM_DRAW_LINE);
+    trajectoryHandler->setiCoordinates(x1, y1, x2, y2);
+    trajectoryHandler->setDrawingValues(1, GL_POINTS, 1, 1, 1);
+    glInterface->setInstance(trajectoryHandler);
+    glInterface->setView2D(GL_PROJECTION, 0, DEF_WINDOW_SIZE, 0, DEF_WINDOW_SIZE);
+
+    // THEN
+    glInterface->startFramework(argc, argv);
+}
+
+void givenX1Y1X2Y2_whenBresenhamAlgorithmAndSlopeIsUndefined_thenLineDraws(int argc, char* argv[]) {
+    // GIVEN
+    TrajectoryHandler* trajectoryHandler = new TrajectoryHandler();
+    GLInterface* glInterface = new GLInterface();
+    int x1 = 100; int y1 = 100;
+    int x2 = 100; int y2 = 400;
+
+    // WHEN
+    // TrajectoryHanler automatically computes the slope at ddaDrawLine()
+    trajectoryHandler->setAlgorithm(ALG_BRSNHM_DRAW_LINE);
+    trajectoryHandler->setiCoordinates(x1, y1, x2, y2);
+    trajectoryHandler->setDrawingValues(1, GL_POINTS, 1, 1, 1);
+    glInterface->setInstance(trajectoryHandler);
+    glInterface->setView2D(GL_PROJECTION, 0, DEF_WINDOW_SIZE, 0, DEF_WINDOW_SIZE);
 
     // THEN
     glInterface->startFramework(argc, argv);
 }
 
 int main(int argc, char* argv[]) {
-    givenX1Y1X2Y2_whenMIsEqualUnder1_thenLineDraws(argc, argv);
+    // ***************** GEOMETRIC ALGORITHM TESTS *****************
+    //givenX1Y1X2Y2_whenGeometricAlgorithmAndSlopeIs0_thenLineDraws(argc, argv);
+
+    // ***************** DDA ALGORITHM TESTS *****************
+    //givenX1Y1X2Y2_whenDdaAlgorithmAndSlopeIs0_thenLineDraws(argc, argv);
+    //givenX1Y1X2Y2_whenDdaAlgorithmAndSlopeIsBetween0And1_thenLineDraws(argc, argv);
+    //givenX1Y1X2Y2_whenDdaAlgorithmAndSlopeIs1_thenLineDraws(argc, argv);
+    //givenX1Y1X2Y2_whenDdaAlgorithmAndSlopeIsMoreThan1_thenLineDraws(argc, argv);
+    //givenX1Y1X2Y2_whenDdaAlgorithmAndSlopeIsUndefined_thenLineDraws(argc, argv);
+
+    // ***************** BRESENHAM ALGORITHM TESTS *****************
+    //givenX1Y1X2Y2_whenbBresenhamAlgorithmAndSlopeIs0_thenLineDraws(argc, argv);
+    //givenX1Y1X2Y2_whenBresenhamAndSlopeIsBetween0And1_thenLineDraws(argc, argv);
+    //givenX1Y1X2Y2_whenBresenhamAlgorithmAndSlopeIs1_thenLineDraws(argc, argv);
+    //givenX1Y1X2Y2_whenBresenhamAlgorithmAndSlopeIsMoreThan1_thenLineDraws(argc, argv);
+    //givenX1Y1X2Y2_whenBresenhamAlgorithmAndSlopeIsUndefined_thenLineDraws(argc, argv);
     return 0;
 }
